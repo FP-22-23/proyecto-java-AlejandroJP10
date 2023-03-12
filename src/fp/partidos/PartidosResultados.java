9a752ec6848282b31bed048bd182fee597c40588
AlejandroJP10 <alegol6club@gmail.com>
2023-03-12 23:35:28 +0100

    Entrega 1 Alejandro Jiménez Pérez

---
+++ src/fp/partidos/PartidosResultados.java
@@ -0,0 +1,45 @@
+package fp.partidos;
+import java.time.Duration;
+
+	public class PartidosResultados {
+		private Integer id;
+		private Integer temporada;
+		private String ciudad;
+		private LocalDateTime fecha;
+		private String equipo1;
+		private String equipo2;
+		private String ganadorSorteo;
+		private String resultadoSorteo;
+		private String ganadorPartido;
+		private Integer puntosRuns;
+		private Integer puntosWickets;
+		
+		//Constructores 
+	    public PartidosResultados(Integer id,Integer temporada,String ciudad,LocalDateTime fecha, String equipo1,String equipo2,
+				String ganadorSorteo,String resultadoSorteo,String ganadorPartido,Integer puntosRuns,Integer puntosWickets) {
+			this.id=id;
+			this.temporada=temporada;
+			this.ciudad=ciudad;
+			checkCiudad(ciudad);
+			this.fecha=fecha;
+			this.equipo1=equipo1;
+			this.equipo2=equipo2;
+			this.ganadorSorteo=ganadorSorteo;
+			this.ganadorPartido=ganadorPartido;
+			this.puntosRuns=puntosRuns;
+			checkPuntosRuns(puntosRuns);
+			this.puntosWickets=puntosWickets;
+			
+		}
+		//Checkers
+		
+		private void checkCiudad(String ciudad) {
+			//El valor almacenado en ciudad no debe estar vacío
+			Checkers.check("El nombre de la ciudad no puede estar vacío",ciudad!=null);
+			
+		}
+		private void checkPuntosRuns(Integer puntosRuns) {
+			//
+		}
+
+}
